35cbdfae8e2cd65cdc30c601abcdba3d5d37a678
Matthew Knight <knightpmatthew@gmail.com>
2019-01-12 21:50:44 +1300

    Fixing Org schema to support listings

--- server/organisation/Organisation.js
+++ server/organisation/Organisation.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+
+const Listing = new mongoose.Schema({
+    name: String,
+    location: String,
+    datePosted: Date,
+});
 const OrganisationSchema = new mongoose.Schema({
     orgName: String,
     location: String,
     email: String,
-    password: String
+    password: String,
+    token: String,
+    listings: [Listing]
 });
+
 mongoose.model('Organisation', OrganisationSchema);
 module.exports = mongoose.model('Organisation');
